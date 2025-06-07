28e86c998f0c2b56358c1cf79eb3be80f9106c0a
jin-sung-oh <wlsqoren@naver.com>
2025-06-07 12:23:19 +0900

    refactor:코멘ㄴ트수정

--- src/main/java/com/vybz/aggregation_service/kafka/config/CommonKafkaConfig.java
+++ src/main/java/com/vybz/aggregation_service/kafka/config/CommonKafkaConfig.java
@@ -27,6 +27,9 @@ public class CommonKafkaConfig {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        /*
+        이부분있으면 FollowCountEvent 로만 매핑을 해줌
+         */
 //        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, FollowCountEvent.class);
 
         return props;
